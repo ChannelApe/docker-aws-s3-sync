7ea5284d6081cbbcad8cc2565bdb6ef13dfd021d
Mike Averto <mjaverto@otreva.com>
2018-10-10 13:13:22 -0400

    Add in envvars for throttleing
    https://stackoverflow.com/questions/30620402/throttling-s3-commands-with-aws-cli

--- sync.sh
+++ sync.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "$(date) - Start"
 
+aws configure set default.s3.max_concurrent_requests $MAX_CONCURRENT_REQUESTS
+aws configure set default.s3.multipart_threshold $MULTIPART_THRESHOLD
+aws configure set default.s3.multipart_chunksize $MULTIPART_CHUNKSIZE
 aws s3 sync /data s3://$BUCKET$BUCKET_PATH $PARAMS
 
 echo "$(date) End"
